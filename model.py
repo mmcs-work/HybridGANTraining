ea02b812c6cb40b351d3e9fbd28002169c3b1fc2
Chendi Qian <chendi.qian@tum.de>
2020-11-04 22:04:17 +0100

    modify model

--- model.py
+++ model.py
@@ -12,33 +12,41 @@ class RealNVP(nn.Module):
 
         self.net_t = nn.Sequential(nn.Linear(mask.shape[0], 1000), 
                                   nn.ReLU(),
-                                  nn.Linear(1000, 28 * 28 - mask.shape[0]))
+                                  nn.Linear(1000, mask.shape[0]))
         self.net_s = nn.Sequential(nn.Linear(mask.shape[0], 1000), 
                                   nn.ReLU(),
-                                  nn.Linear(1000, 28 * 28 - mask.shape[0]))
+                                  nn.Linear(1000, mask.shape[0]))
 
     def forward(self, x):
         x = x.reshape(x.shape[0], -1)
-        x1 = x[:, self.mask]
-        x2 = x[:, np.setdiff1d(np.arange(x.shape[1]), self.mask)]
-        t = self.net_t(x1)
-        s = self.net_s(x1)
-        y1 = x1
-        y2 = torch.exp(s) * x2 + t
-        y = torch.cat((y1, y2), dim=1)
-        log_det_j = torch.sum(s)
+        # x1 = x[:, self.mask]
+        # x2 = x[:, np.setdiff1d(np.arange(x.shape[1]), self.mask)]
+        # t = self.net_t(x1)
+        # s = self.net_s(x1)
+        # y1 = x1
+        # y2 = torch.exp(s) * x2 + t
+        # y = torch.cat((y1, y2), dim=1)
+        # log_det_j = torch.sum(s)
+        t = self.net_t(self.mask * x)
+        s = self.net_s(self.mask * x)
+        y = self.mask * x + (1 - self.mask) * (x * torch.exp(s) + t)
+        log_det_j = torch.sum((1 - self.mask) * s)
         return y, log_det_j
-    
+
     def inverse(self, y):
         y = y.reshape(y.shape[0], -1)
-        y1 = y[:, self.mask]
-        y2 = y[:, np.setdiff1d(np.arange(y.shape[1]), self.mask)]
-        t = self.net_t(y1)
-        s = self.net_s(y1)
-        x1 = y1
-        x2 = (y2 - t) * torch.exp(-s)
-        x = torch.cat((x1, x2), dim=1)
-        log_det_inv_j = torch.sum(-s)
+        # y1 = y[:, self.mask]
+        # y2 = y[:, np.setdiff1d(np.arange(y.shape[1]), self.mask)]
+        # t = self.net_t(y1)
+        # s = self.net_s(y1)
+        # x1 = y1
+        # x2 = (y2 - t) * torch.exp(-s)
+        # x = torch.cat((x1, x2), dim=1)
+        # log_det_inv_j = torch.sum(-s)
+        t = self.net_t(self.mask * y)
+        s = self.net_s(self.mask * y)
+        x = self.mask * y + (1 - self.mask) * ((y - t) * torch.exp(-s))
+        log_det_inv_j = torch.sum((1 - self.mask) * (-s))
         return x, log_det_inv_j
     
     def log_prob(self, y):
