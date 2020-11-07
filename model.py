be5080a55286e499b7e9c8b4cdf1dbb3fc6381bd
mainakmanna <moinak.manna@gmail.com>
2020-11-07 10:04:09 +0100

    Adds resnet code for RealNVP

--- model.py
+++ model.py
@@ -1,7 +1,58 @@
 import torch
 import torch.nn as nn
 import numpy as np
+import torch.nn.functional as F
 
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(ResidualBlock, self).__init__()
+
+        self.bn1 = nn.BatchNorm2d(in_channels)
+        
+        self.conv1 = nn.utils.weight_norm(nn.Conv2d(in_channels, out_channels, kernel_size, padding=padding, bias=False))
+
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.utils.weight_norm(nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, bias=True))
+
+    def forward(self, x):
+        skipcon = x
+        x = self.conv1(F.relu(self.bn1(x)))
+        x = self.conv2(F.relu(self.bn2(x)))
+        x = x + skipcon
+
+        return x
+
+class ResNet(nn.Module):
+    def __init__(self, in_channels, mid_channels, out_channels,
+                 num_blocks, kernel_size, padding):
+        super(ResNet, self).__init__()
+        self.bn1 = nn.BatchNorm2d(in_channels)
+        self.conv1 = nn.utils.weight_norm(nn.Conv2d((2 * in_channels, mid_channels, kernel_size, padding, bias=True))
+        self.skip1 = nn.utils.weight_norm(nn.Conv2d((mid_channels, mid_channels, kernel_size=1, padding=0, bias=True))
+
+        self.blocks = nn.ModuleList([ResidualBlock(mid_channels, mid_channels)
+                                     for _ in range(num_blocks)])
+        self.skips = nn.ModuleList([nn.utils.weight_norm(nn.Conv2d(mid_channels, mid_channels, kernel_size=1, padding=0, bias=True))
+                                    for _ in range(num_blocks)])
+
+        self.bn2 = nn.BatchNorm2d(mid_channels)
+        self.conv2 = nn.utils.weight_norm(nn.Conv2d((mid_channels, out_channels, kernel_size=1, padding=0, bias=True))
+
+    def forward(self, x):
+        x = self.bn1(x)
+        x = torch.cat((x, -x), dim=1)
+        x = self.conv1(F.relu(x))
+        x_skip = self.skip1(x)
+
+        for block, skip in zip(self.blocks, self.skips):
+            x = block(x)
+            x_skip += skip(x)
+
+        x = self.bn2(x_skip)
+        x = F.relu(x)
+        x = self.conv2(x)
+
+        return x
 
 class RealNVP(nn.Module):
     def __init__(self, mask, base_dist):
